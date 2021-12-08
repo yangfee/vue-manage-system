e90a4bf5aa355409855ed177e6d68164fd5f8fca
a02456 <li.yang@casstime.com>
2021-12-08 11:50:18 +0800

    feat(global) 配置别名 @ => src

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,30 @@
-import vue from '@vitejs/plugin-vue'
+import path from "path";
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
-export default {
-    base: './',
-    plugins: [vue()],
-    optimizeDeps: {
-        include: ['schart.js']
-    }
-}
+export default defineConfig({
+  base: "./",
+  plugins: [vue()],
+  optimizeDeps: {
+    include: ["schart.js"],
+  },
+  resolve: {
+    alias: [
+      {
+        // @/a/b === src/a/b
+        find: "@/",
+        replacement: `${path.resolve(__dirname, "src")}/`,
+      },
+      {
+        // ~/a/b === src/a/b
+        find: "~/",
+        replacement: `${path.resolve(__dirname, "src")}/`,
+      },
+      {
+        // ~mode  === node_modules/mode
+        find: /~([^\/]+)/,
+        replacement: `${path.resolve(__dirname, "node_modules/$1")}/`,
+      },
+    ],
+  },
+});
